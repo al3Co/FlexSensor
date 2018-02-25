0d5d0e2f74ecca0d77673599c994bedc9501aab9
AldoContreGo <aldocontrego@gmail.com>
2018-02-25 13:48:37 +0100

    code Improvements
    code Improvements

--- readingTestsFiles.m
+++ readingTestsFiles.m
@@ -4,31 +4,31 @@ clear
 clc
 %% Read and categorice data
 
-filename1 = 'AFCG_Green_Fron_2018-02-23T115847460.xlsx';
-filename2 = 'AFCG_Green_Side_2018-02-23T121906523.xlsx';
-filename3 = 'AFCG_Green_Side_2018-02-23T123450039.xlsx';
-filename4 = 'AFCG_Green_Side_2018-02-23T125059133.xlsx';
-filename5 = 'AFCG_Green_Side_2018-02-23T130301997.xlsx';
-filename6 = 'AFCG_Green_Side_2018-02-23T132000752.xlsx';
-filename7 = 'AFCG_Green_Side_2018-02-23T133414181.xlsx';
-filename8 = 'AFCG_Green_Side_2018-02-23T135820861.xlsx';
-filename9 = 'AFCG_Green_Side_2018-02-23T151949001.xlsx';
-filename10 = 'AFCG_Green_Side_2018-02-23T155657865.xlsx';
-filename11 = 'AFCG_Green_Side_2018-02-23T164205229.xlsx';
-filename12 = 'AFCG_Green_Side_2018-02-23T170422681.xlsx';
+JaimeTFM = 'AFCG_Green_Fron_2018-02-23T115847460.xlsx';
+Edu = 'AFCG_Green_Side_2018-02-23T121906523.xlsx';
+Carlos = 'AFCG_Green_Side_2018-02-23T123450039.xlsx';
+OtroLAT  = 'AFCG_Green_Side_2018-02-23T125059133.xlsx';
+Roomie1  = 'AFCG_Green_Side_2018-02-23T130301997.xlsx';
+Sonia  = 'AFCG_Green_Side_2018-02-23T132000752.xlsx';
+Pablo  = 'AFCG_Green_Side_2018-02-23T133414181.xlsx';
+Roomie2  = 'AFCG_Green_Side_2018-02-23T135820861.xlsx';
+Brenosa = 'AFCG_Green_Side_2018-02-23T151949001.xlsx';
+Prima = 'AFCG_Green_Side_2018-02-23T155657865.xlsx';
+Jaime = 'AFCG_Green_Side_2018-02-23T164205229.xlsx';
+Doris = 'AFCG_Green_Side_2018-02-23T170422681.xlsx';
 
-T1 = readtable(filename1);
-T2 = readtable(filename2);
-T3 = readtable(filename3);
-T4 = readtable(filename4);
-T5 = readtable(filename5);
-T6 = readtable(filename6);
-T7 = readtable(filename7);
-T8 = readtable(filename8);
-T9 = readtable(filename9);
-T10 = readtable(filename10);
-T11 = readtable(filename11);
-T12 = readtable(filename12);
+T1 = readtable(Carlos);
+T2 = readtable(OtroLAT);
+T3 = readtable(Roomie1);
+T4 = readtable(Sonia);
+T5 = readtable(Pablo);
+T6 = readtable(Roomie2);
+T7 = readtable(Brenosa);
+T8 = readtable(Prima);
+T9 = readtable(Doris);
+T10 = readtable(Aldo);
+%T11 = readtable(filename11);
+%T12 = readtable(filename12);
 
 
 
