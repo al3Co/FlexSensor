1d1d5a85a4f4b80d6e28ae4d67387e1b3566b9ee
AldoContreGo <aldocontrego@gmail.com>
2018-03-07 08:03:37 +0100

    comments added
    comments added

--- Jaime TFM/OptiTrackData.m
+++ Jaime TFM/OptiTrackData.m
@@ -3,12 +3,13 @@ close all
 clear
 clc
 %% 
-
+% get data as table
 T = readtable('C:\Users\Aldo Contreras\Documents\ExoFlex\Jaime_TFM\OptiTrack\GREEN-FRONT2.csv');
 
 % data = str2double([T.RigidBody(5:end) T.RigidBody_1(5:end) T.RigidBody_2(5:end) T.RigidBody_7(5:end) T.RigidBody_8(5:end) T.RigidBody_9(5:end)]);
 data = str2double([T.RigidBody_3(5:end) T.RigidBody_4(5:end) T.RigidBody_5(5:end) T.RigidBody_10(5:end) T.RigidBody_11(5:end) T.RigidBody_12(5:end) T.Marker_18(5:end) T.Marker_19(5:end) T.Marker_20(5:end)]);
 
+% if nan value on matrix, nan changes for previous immediate value
 for col=1:9
     for i = 1:length(data)
       if isnan(data(i, col))
@@ -17,4 +18,5 @@ for col=1:9
     end
 end
 
+% plotting "brazo", "espalda" and "referencia" position
 plot(data)
