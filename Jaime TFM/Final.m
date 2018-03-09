da9a8761126ec7ebe9cdf955c475e73c1f2f44c6
AldoContreGo <aldocontrego@gmail.com>
2018-03-09 14:51:16 +0100

    data added
    data added

--- Jaime TFM/Final.m
+++
@@ -1,9 +0,0 @@
-prompt = ('Enter sensor data file name: ');
-SensorFile = input(prompt,'s');
-prompt = ('Enter optitrack data file name: ');
-ficherocsv = input(prompt,'s');
-
-[ArmPos,ArmAngle,Reference,Verticality,RealTime] = OptiCoord(ficherocsv);
-[X,Y,Z,XV,YV,ZV] = QuaternTimeCheck(SensorFile,ArmAngle,Verticality,RealTime);
-BrazoGF = [X,Y,Z];
-EspaldaGF = [XV,YV,ZV];
--- Jaime TFM/STCorrection.m
+++
@@ -1,8 +0,0 @@
-%Sensor time correction
-function [SensorTime] = STCorrection(SensorFile)
-
-x = readtable(SensorFile);
-
-SensorTime = seconds(hours(x.Hour) + minutes(x.Minute) + seconds(x.Sec));
-
-end
