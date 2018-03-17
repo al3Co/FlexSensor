e53778050b07becdcc27e9f8ba87e2710b3b0a0a
Aldo Contreras <aldocontrego@gmail.com>
2018-03-17 09:37:28 +0100

    Arduino code upload
    Arduino code upload

---
+++ Arduino/read_sendToMatlab/read_sendToMatlab.ino
@@ -0,0 +1,17 @@
+const int nSensors = 9;   // including zero
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int sensorPin = 0; sensorPin <= nSensors; sensorPin++) {
+    float inputVoltage = analogRead(sensorPin);
+    float voltage = ((5 * inputVoltage)/1024);
+    if (sensorPin < nSensors) {
+        Serial.print(voltage);
+        Serial.print(",");
+    }
+    else{Serial.println(voltage);}
+  }
+}
