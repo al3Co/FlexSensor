d126becfc2512c00e08d1c31b3b8e63ba5aa02f1
Aldo Contreras <aldocontrego@gmail.com>
2018-04-23 16:16:26 +0200

    first commit
    first commit

---
+++ DrawMovements.m
@@ -0,0 +1,10 @@
+% 23.04.18
+% draw movements from quaternions
+
+load('all_27.mat')
+[yaw, pitch, roll] = quat2angle([Q1 Q2 Q3 Q4]);
+
+for i=1:length(yaw)
+    funcPlotVector(pitch(i), roll(i), yaw(i))
+    % pause(0.1)
+end
