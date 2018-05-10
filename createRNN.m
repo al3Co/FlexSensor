c4afda630725479d473c776f4b4b0e981d54c097
AldoContreGo <aldocontrego@gmail.com>
2018-05-10 21:12:34 +0200

    plot input-target
    plot input-target

--- createRNN.m
+++ createRNN.m
@@ -2,9 +2,13 @@
 clear
 close all
 clc
-
+%% data
 [kindOfData, kindOfTest] = usrDataFunc ();
 [WorkSpace] = loadWSFunc(kindOfData);
 [input, target] = dataToRNN(WorkSpace, kindOfTest);
+plot(input)
+hold on
+plot(target)
+%% RNN
 [performanceTot] = rnnFunc(input, target);
 disp(performanceTot)
