1b01589c6e99a56f390461b9ac2b4c2edc20060a
Aldo Contreras <aldocontrego@gmail.com>
2018-06-06 19:29:25 +0200

    plot title
    plot title

--- Functions/funcPlotMultFlexSens.m
+++ Functions/funcPlotMultFlexSens.m
@@ -2,7 +2,7 @@ function funcPlotMultFlexSens(voltageA0, T)
 % Plot
 dataPlot = [voltageA0];
 plot(1:T,dataPlot)
-%title(sprintf('A0:', voltageA0))
+title('Calibration: Move the arm')
 grid on;
 xlabel('Samples') % x-axis label
 ylabel('Voltage') % y-axis label
