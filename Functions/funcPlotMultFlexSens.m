d59f9dee912ac3d71a61158b3ea66012b046130d
Aldo Contreras <aldocontrego@gmail.com>
2018-06-07 11:41:10 +0200

    faster plot
    faster plot

--- Functions/funcPlotMultFlexSens.m
+++ Functions/funcPlotMultFlexSens.m
@@ -2,9 +2,9 @@ function funcPlotMultFlexSens(voltageA0, T)
 % Plot
 dataPlot = [voltageA0];
 plot(1:T,dataPlot)
-title('Calibration: Move the arm')
+% title('Calibration: Move the arm')
 grid on;
-xlabel('Samples') % x-axis label
-ylabel('Voltage') % y-axis label
+% xlabel('Samples') % x-axis label
+% ylabel('Voltage') % y-axis label
 drawnow
 end
