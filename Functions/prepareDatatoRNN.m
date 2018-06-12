fc1cc623d825843b58b7eb785d0351487d3b8d84
Aldo Contreras <aldocontrego@gmail.com>
2018-06-12 12:02:15 +0200

    update data
    update data

--- Functions/prepareDatatoRNN.m
+++ Functions/prepareDatatoRNN.m
@@ -5,26 +5,25 @@ function [x, xi] = prepareDatatoRNN(input, target)
 %                   0.609778,-0.304149,0.497088,-0.537185]
     % in = input(1,:);
     % tar = target(1,:);
-    x = cell(2,0);
+    % x = cell(2,0);
     % xi = {input';target'};
-    xi = [tonndata(input,false,false); tonndata(target,false,false)];
+    % xi = [tonndata(input,false,false); tonndata(target,false,false)];
     
     
-%     X = tonndata(input,false,false);
-%     T = tonndata(target,false,false);
-% 
-%     trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
-% 
-%     % Create a Nonlinear Autoregressive Network with External Input
-%     inputDelays = 1:2;
-%     feedbackDelays = 1:2;
-%     hiddenLayerSize = 10;
-%     net = narxnet(inputDelays,feedbackDelays,hiddenLayerSize,'open',trainFcn);
-% 
-%     net.inputs{1}.processFcns = {'removeconstantrows','mapminmax'};
-%     net.inputs{2}.processFcns = {'removeconstantrows','mapminmax'};
-% 
-%     [x,xi,ai,t] = preparets(net,X,{},T);
+X = tonndata(input,false,false);
+T = tonndata(target,false,false);
+
+trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
+
+inputDelays = 1:2;
+feedbackDelays = 1:2;
+hiddenLayerSize = 10;
+net = narxnet(inputDelays,feedbackDelays,hiddenLayerSize,'open',trainFcn);
+
+net.inputs{1}.processFcns = {'removeconstantrows','mapminmax'};
+net.inputs{2}.processFcns = {'removeconstantrows','mapminmax'};
+
+[x,xi,ai,t] = preparets(net,X,{},T);
 end
 
 
