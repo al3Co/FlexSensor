09dbd860fdf21d8bb205f326574cc5e7bf9718fe
Aldo Contreras <aldocontrego@gmail.com>
2018-06-12 18:41:19 +0200

    plot angles offline
    plot angles offline

--- Functions/funcPlotAnglesResults.m
+++ Functions/funcPlotAnglesResults.m
@@ -2,12 +2,14 @@
 % data needed:
 % initial angles [X,Y,Z]
 % angles from results [X,Y,Z]
-% length = 1
+% length = 10
+load('C:\Users\Aldo Contreras\Documents\GitHub\FlexSensor\testsData\test110618N.mat')
 
-L1 = 10; % length
+L1 = 1; % length
 
 %% add here for loop 
 for rawLoop = 1:length(angN)
+    clf;
     % from angle and length, get position
     theta1 = angN(rawLoop,1);
     theta2 = angN(rawLoop,2);
@@ -32,31 +34,42 @@ for rawLoop = 1:length(angN)
     P30 = R30 * P12;        % Position point
 
     % plot data
+    grid on;
     hold all;
     grid on;
     subplot(2,2,1);
-    mArrow3([0 0 0],[P30(1) P30(2) P30(3)], 'facealpha', 0.5, 'color', 'blue', 'stemWidth', 0.02);
-    %set(gca, 'CameraPosition', [6 2 3]);
+    mArrow3([0 0 0],[P30(1) P30(2) P30(3)], 'color', 'red', 'stemWidth', 0.015);
+    axis([-1 1 -1 1 -1 1]);
     view(3);
     
+    %2
     subplot(2,2,2);
     hold all;
+    grid on;
     plot([0;P30(1)],[0;P30(2)],'r');
+    axis([-1 1 -1 1])
     view(0,90);
     
+    %3
     subplot(2,2,3);
     hold all;
+    grid on;
     plot([0;P30(1)],[0;P30(3)],'r');
+    axis([-1 1 -1 1])
     view(0,90);
     
+    %4
     subplot(2,2,4);
     hold all;
+    grid on;
     plot([0;P30(2)],[0;P30(3)],'r');
+    axis([-1 1 -1 1])
     view(0,90);
+    
     if rawLoop == 1
-       pause(2)
+       pause(0.05)
     else
-        pause(0.001)
+        pause(0.05)
     end
 end
 
