c8bf3d5fdcd09df9fb98844fa559c36606acc7f4
AldoContreGo <aldocontrego@gmail.com>
2018-09-05 18:15:18 +0200

    improve get data from serial port
    improve get data from serial port

--- Arduino/serialLily/getSensorsData.py
+++ Arduino/serialLily/getSensorsData.py
@@ -9,29 +9,37 @@ Created on Tue Jul 17 11:04:25 2018
 import time
 import datetime
 import serial
-import sys 
+import sys
 
 filename = time.strftime("%Y%m%d-%H%M%S")
 
 try:
-    ser = serial.Serial('COM3', 115200)
+    ser = serial.Serial('/dev/tty.usbserial-FT9028B4', 115200)
 except:
     sys.exit('review the serial port')
- 
+
 rawdata = []
 
 def getData():
     file = open((filename + '.txt'), mode = 'w')
     nCount = 1
     tic = time.time()
+    actual = 0;
     ser.inWaiting()
-    while(nCount<=10):
+    while(nCount<=1000):
         ser.flush()
-        print('\rCount: {} Sensors: {}'.format(nCount, ser.readline().strip()),
-            sep='\n', end='', flush=True)
-        rawdata.append((ser.readline().strip()))
-        file.write(str(nCount) + ',' + str(datetime.datetime.now().time()) + ',' + str(ser.readline().strip()) + '\n')
-        nCount += 1
+        try:
+            read = ser.readline().decode('utf-8').strip()
+            elapsed = int(time.time() - tic)
+            if actual != elapsed:
+                actual = elapsed
+                print('\rElapsed Time: {}'.format(elapsed), sep='\n', end='', flush=True)
+            rawdata.append(read)
+            if nCount > 10:
+                file.write(str(nCount-10) + ',' + str(datetime.datetime.now().time()) + ',' + str(read) + '\n')
+            nCount += 1
+        except UnicodeDecodeError:
+            pass
     toc = time.time()
     ser.close()
     file.close()
--- Arduino/serialLily/serialLily.py
+++ Arduino/serialLily/serialLily.py
@@ -2,24 +2,24 @@
 # read and measure the transmission speed of serial (Lily) data
 import time
 import serial
-import sys 
+import sys
 
 try:
-    ser = serial.Serial('COM3', 115200)
+    ser = serial.Serial('/dev/tty.usbserial-FT9028B4', 115200)
 except:
     sys.exit('review the serial port')
 
 rawdata = []
 nCount = 0
 
-tic = time.clock()
+tic = time.time()
 while(nCount<100):
     ser.flush()
-    print('\rCount: {} Sensors: {}'.format(nCount, ser.readline().strip()),
+    print('\rCount: {} Sensors: {}'.format(nCount, ser.readline().decode('utf-8').strip()),
         sep='\n', end='', flush=True)
-    rawdata.append(ser.readline().strip())
+    rawdata.append(ser.readline().decode('utf-8').strip())
     nCount += 1
 
-toc = time.clock()
+toc = time.time()
 print('\nFrequency: {} Hz'.format(nCount/(toc - tic)))
 ser.close()
